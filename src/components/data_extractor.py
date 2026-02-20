87d4aff1b7476109e08687c64cb9457c7e1e04ab
MarkHague <markhg77@gmail.com>
2026-02-20 12:26:30 +0100

    Updates to data_extractor.py - chaining of methods together in run)extraction + bug fixes.

--- src/components/data_extractor.py
+++ src/components/data_extractor.py
@@ -18,8 +18,9 @@ class DataExtractConfig:
     PASSWORD: str = os.getenv('OXYLABS_PASSWORD')
     API_URL: str = 'https://realtime.oxylabs.io/v1/queries'
 
-    SAVE_PATH : str = '../../artifacts/data/'
-
+    DATA_DIR : str = os.path.normpath(
+        os.path.join( os.path.dirname(__file__),'..', '..', 'artifacts', 'data')
+    )
 
 class DataExtractor:
 
@@ -30,12 +31,14 @@ class DataExtractor:
     def get_image_data_df(self, query: str = None, pages: int = 1,
                           expected_label: str = "dirty") -> pd.DataFrame:
         """
-        Returns a pd.Dataframe containing image urls from Oxylabs API given search params.
+        Retrieve image data from Oxylabs API given search params.
 
         Args:
             query: Search query for Google image search.
             pages: Number of pages to scrape. Each page is 100 images.
             expected_label: The label you expect given the query to Oxylabs image search API.
+        Returns:
+            A pandas DataFrame with the image urls and other useful metadata related to each image.
 
         """
 
@@ -48,7 +51,7 @@ class DataExtractor:
                 'query': query,
                 'parse': True,
                 'pages': pages,
-                'start_page': page,
+                'start_page': page+1,
                 'context': [
                     {'key': 'tbm', 'value': 'isch'},
                 ],
@@ -61,13 +64,19 @@ class DataExtractor:
                 json=payload,
             )
 
-            data_json = response.json()
+            if response.status_code == 200:
+                data_json = response.json()
 
-            df = self.convert_json_to_csv(json_data= data_json, expected_label=expected_label)
-            data_df = pd.concat([data_df, df])
+                df = self.convert_json_to_csv(json_data= data_json, expected_label=expected_label)
+                data_df = pd.concat([data_df, df])
 
-        data_df.index = range(len(data_df))
-        return data_df
+                data_df.index = range(len(data_df))
+
+            else:
+                logging.info(f"ERROR. Image data extraction failed with response code {response.status_code}, on page {page}")
+
+        if len(data_df) > 0:
+            return data_df.drop_duplicates(subset=["image_url"])
 
 
     def convert_json_to_csv(self, json_data : dict = None, expected_label: str = "dirty") -> pd.DataFrame:
@@ -89,24 +98,34 @@ class DataExtractor:
 
         df_out = pd.DataFrame(columns=column_names)
 
-        for img in json_data_res:
-            new_row = {'date_extracted': date_extracted,
-                       'label': label,
-                       'domain': img["domain"],
-                       'image_url': img["high_res_image"],
-                       'image_title': img["title"],
-                       'page_number': page_num,
-                       'query': query
-                       }
+        try:
+            for img in json_data_res:
+                new_row = {'date_extracted': date_extracted,
+                           'label': label,
+                           'domain': img["domain"],
+                           'image_url': img["high_res_image"],
+                           'image_title': img["title"],
+                           'page_number': page_num,
+                           'query': query
+                           }
 
-            df_out.loc[len(df_out)] = new_row
+                df_out.loc[len(df_out)] = new_row
+        except Exception as e:
+            raise CustomException(e, sys)
+
 
         return df_out
 
-    def extract_images(self, image_data: pd.DataFrame = None) -> pd.DataFrame | None:
-
+    def save_images(self, image_data: pd.DataFrame = None) -> pd.DataFrame | None:
         """
-        Extracts (saves to disk) image files from a list of image urls.
+        Saves image files to disk using an input dataframe containing the image urls.
+
+        Args:
+            image_data: pandas Dataframe created by get_image_data_df method.
+
+        Returns:
+            pandas Dataframe containing any abnormal response codes (!= 200), and associated data.
+            If no abnormal response codes are present returns None.
         """
         cols = ["image_name","image_url", "response_code"]
         abnormal_response = pd.DataFrame(columns=cols)
@@ -147,8 +166,13 @@ class DataExtractor:
                     img = Image.open(io.BytesIO(response.content))
                     img_ext = "." + img.format.lower()
 
-                    filepath = os.path.join(self.data_extract_config.SAVE_PATH, "images", row['label'],
+                    filepath = os.path.join(self.data_extract_config.DATA_DIR, "images", row['label'],
                                             row['query'] + "_" + str(index) + img_ext)
+                    # make base save path if not already done
+                    base_path = os.path.join(self.data_extract_config.DATA_DIR, "images", row['label'])
+                    if not os.path.isdir(base_path):
+                        os.makedirs(base_path)
+
                     with open(filepath, 'wb') as file:
                         file.write(response.content)
                         logging.info(f"Writing to disk: {file}")
@@ -163,6 +187,42 @@ class DataExtractor:
 
         if len(abnormal_response) > 0:
             return abnormal_response
+        else:
+            return None
+
+    def save_df_to_csv(self, dataframe: pd.DataFrame = None, file_name: str = "data.csv") -> None:
+        file_path = os.path.join(self.data_extract_config.DATA_DIR, file_name)
+        dataframe.to_csv(file_path)
+
+    def run_extraction(self, queries: dict[list] = None, pages_per_query = 2,
+                       filename_output_csv_data: str = 'data.csv'):
+        """
+        Run all steps required to extract image data using the Oxylabs API.
+
+        Args:
+            queries: A dict where each label is a key, and its values are a list of search queries.
+            pages_per_query: Number of pages per query. Each page contains 100 images.
+            filename_output_csv_data: File name of the output data containing all the image urls and meta-data.
+        """
+        logging.info(f"RUNNING IMAGE EXTRACTION WITH QUERY DICT: {queries}")
+        # 1. Get the image urls and other meta-data for each query and combine
+        df_list = []
+        for label, query_list in queries.items():
+            for qry in query_list:
+                df_list.append(self.get_image_data_df(query = qry, pages=pages_per_query, expected_label=label) )
+
+        df_all_data = pd.concat(df_list).drop_duplicates(subset=['image_url'] )
+        df_all_data.index = range(len(df_all_data))
+        # write data to DATA_DIR
+        logging.info(f"Saving image data csv file as {filename_output_csv_data}")
+        self.save_df_to_csv(df_all_data, file_name=filename_output_csv_data)
+
+        # 2. Extract images from df_all_data
+        abnormal_responses = self.save_images(df_all_data)
+
+        return abnormal_responses
+
+
 
 
 
