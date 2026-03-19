26e265f35122bb7823a64e18f094f7747bedfd3c
MarkHague <markhg77@gmail.com>
2026-03-19 14:03:08 +0100

    Minor changes related to docker.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 import flask
 from src.components.deploy_model import DeployModel
+import os
 
 # load the model here before launching the server
+
 deploy = DeployModel(
-    model_save_path='../artifacts/training',
+    model_save_path=os.environ.get('MODEL_PATH', '../artifacts/training'),
     model_name='model_mobnet_curated_data_extended'
 )
+
 app = flask.Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
@@ -13,4 +16,4 @@ def predict(threshold = 0.6):
     return deploy.predict(threshold=threshold)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=80)
